e717f76d8d4975bfc0da2d84d01c862e0a65abc2
Brecht <brechtnulens@gmail.com>
2020-11-06 00:58:04 +0100

    Basic styling for footer starter

---
+++ src/style/elements/StyledFooter.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const StyledFooter = styled.footer`
+    height: 100px;
+    width: 100%;
+    border: 1px solid orange;
+
+    .footer__inner-wrapper {
+        display: flex;
+        align-items: center;
+        width: 80vw;
+        margin: 0 auto;
+    }
+`
