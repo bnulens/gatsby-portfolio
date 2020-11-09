4a60f595e48e910186c207bb80438a7a4b681287
Brecht <brechtnulens@gmail.com>
2020-11-09 21:48:35 +0100

    Passed children to layout element as props

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./src/style/theme/global"
 
-import Theme from "./src/style/theme/theme"
-import Layout from "./src/style/theme/layout"
+import { GlobalStyle } from "./src/style/theme/global";
+import Layout from "./src/style/theme/layout";
+import theme from "./src/style/theme/theme";
 
-
-export const wrapRootElement = () => (
-    <ThemeProvider theme={Theme}>
-        <GlobalStyle/>
-        <Layout/>
-    </ThemeProvider>
-)
+export const wrapRootElement = ({element}) => {
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Layout children={element}/>
+        </ThemeProvider>
+    )
+}
