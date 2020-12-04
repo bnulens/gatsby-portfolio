f6c1e6d89d6a0157cd3599f62266d0e49ea04c40
bnulens <brechtnulens@gmail.com>
2020-12-04 01:39:05 +0100

    Hero comp inserted into Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 import SEO from "../components/seo"
+import Hero from "../components/Hero"
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
+    <Hero/>
     <section>
       <p>Thank y'all for coming</p>
     </section>
