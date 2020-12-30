b36966c4f41729d65b24dc4c5717c4ddbf41daa7
bnulens <brechtnulens@gmail.com>
2020-12-30 15:57:49 +0100

    Added About & Experience comps to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,15 @@ import React from "react"
 
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
+import About from "../components/Content/About"
+import Experience from "../components/Content/Experience"
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
     <Hero/>
-    <section>
-      <p>Thank y'all for coming</p>
-    </section>
+    <About/>
+    <Experience/>
   </>
 )
 
