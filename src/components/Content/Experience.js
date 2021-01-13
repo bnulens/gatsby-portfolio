746c2213ff987f64aaa51ecb98e5da3f89f4d57c
bnulens <brechtnulens@gmail.com>
2021-01-13 15:26:27 +0100

    Adjusted link to example website

--- src/components/Content/Experience.js
+++ src/components/Content/Experience.js
@@ -12,7 +12,7 @@ const Experience = () => {
                     <p>After a couple attempts in finding a proper field of education, I am convinced I now am on the right track. I feel ready to strike and make this the greatest rebound of my life.</p>
                 </div>
                 <div className="experience__links">
-                    <a className="link__record-store" href="https://app.netlify.com/sites/epic-galileo-8a7f26/overview" target="_blank" rel="noopener noreferrer">Record store</a>
+                    <a className="link__record-store" href="https://epic-galileo-8a7f26.netlify.app/" target="_blank" rel="noopener noreferrer">Record store</a>
                     <a className="link__motivation" href="https://unruffled-bartik-c63ef3.netlify.app/" target="_blank" rel="noopener noreferrer">Motivation</a>
                 </div>
             </div>
