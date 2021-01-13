7e5d728ec8d091c1f05f2f10b34bb82f5a50063e
bnulens <brechtnulens@gmail.com>
2021-01-13 15:33:34 +0100

    Hero component - queries hero image from cloudinary

---
+++ src/components/Content/Hero.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+
+const StyledHero = styled.div`
+    position: relative;
+    img {
+        display: block;
+        position: relative;
+        left: 50%;
+        right: 50%;
+        margin-left: -50vw;
+        margin-right: -50vw;
+        width: 100vw;
+        max-width: 100vw;
+        max-height: 100vh;
+        object-fit: cover;
+    }
+    h2 {
+        position: absolute;
+        top: 30%;
+        left: 0;
+        color: var(--color-white);
+        font-size: 4vw;
+        font-weight: 400;
+    }
+`
+
+const Hero = () => {
+    const data = useStaticQuery(graphql`
+        query CloudinaryImage {
+            cloudinaryMedia(public_id: {eq: "gatsby-portfolio/portfolio-hero-2"}) {
+                secure_url
+            }
+        }
+        `
+    );
+    const clImage = data.cloudinaryMedia.secure_url
+    if (!clImage) {
+        return <div className="default-hero">Picture not found</div>
+    }
+
+    return (
+        <StyledHero hasHero>
+            <img src={clImage} alt="cloudinary-hero"/>
+            <h2>Looking for a fast break</h2>
+        </StyledHero>
+    )
+}
+
+export default Hero
