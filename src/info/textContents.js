d28586594b16f5f1516813dcbaef628b7212e37e
bnulens <brechtnulens@gmail.com>
2021-01-30 01:17:11 +0100

    Added objects to ListContent descibing academic feats

--- src/info/textContents.js
+++ src/info/textContents.js
@@ -1,31 +1,68 @@
 export const reviewContent = [
-    {
-        reviewer: "Geert Van Campenhout",
-        comment: "I have worked with Brecht on two projects. He is eager to learn and a high potential. In the short amount of time he has been developing, he has picked up a lot of skills and is on track to become a great addition to any development team.",
-        jobTitle: "Full stack developer",
-        worksAt: "StriveCloud - scalable gamification software",
-        images: {
-            avatar: "geert-lion.jpg",
-            company: "strivecloud-full.png"
-        }
+  {
+    reviewer: "Geert Van Campenhout",
+    comment: "I have worked with Brecht on two projects. He is eager to learn and a high potential. In the short amount of time he has been developing, he has picked up a lot of skills and is on track to become a great addition to any development team.",
+    jobTitle: "Full stack developer",
+    worksAt: "StriveCloud - scalable gamification software",
+    images: {
+      avatar: "geert-lion.jpg",
+      company: "strivecloud-full.png"
     }
+  }
 ];
 
 export const listContent = [
     {
-        tag: "newbie",
-        title: "I am a noob",
-        comment: "Hello"
+      tag: "newbie",
+      title: "I am a noob",
+      comment: "Hello"
     },
     {
-        tag: "experience",
-        title: "Experience",
-        comment: "A quick brown fox jumped over the lazy dog"
+      tag: "education",
+      title: "Academics",
+      comment: "A road less travelled...",
+      studies: [
+        {
+          title: 'Junior Web Developer',
+          year: '2019',
+          place: 'Genk',
+          organistation: 'BeCode',
+          success: true
+        },
+        {
+          title: 'Junior Network Engineer',
+          year: '2018',
+          place: 'Genk',
+          organistation: 'Syntra',
+          success: true
+        },
+        {
+          title: 'Bachelor Graphic Design',
+          year: '2014-2018',
+          place: 'Hasselt',
+          organistation: 'PXL Mad-Faculty',
+          success: false
+        },
+        {
+          title: 'Bachelor Product Design',
+          year: '2013-2014',
+          place: 'Genk',
+          organistation: 'PXL Mad-Faculty',
+          success: false
+        },
+        {
+          title: 'ASO',
+          year: '2007-2013',
+          place: 'Hasselt',
+          organistation: 'Koninklijk Atheneum 1',
+          success: true
+        },
+      ]
     },
     {
-        tag: "extra",
-        title: "Possibility",
-        comment: "A quick brown fox jumped over the lazy dog"
+      tag: "extra",
+      title: "Possibility",
+      comment: "A quick brown fox jumped over the lazy dog"
     },
 ];
 
