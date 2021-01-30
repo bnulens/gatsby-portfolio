1de8d6425481c96cfb719e6cb6c5f334d2e50599
bnulens <brechtnulens@gmail.com>
2021-01-30 13:28:33 +0100

    Adjusted query for single image - centered main title and catch title

--- src/components/Content/Hero.js
+++ src/components/Content/Hero.js
@@ -3,59 +3,72 @@ import { useStaticQuery, graphql } from "gatsby"
 import { useTranslation } from "react-i18next"
 import styled from "styled-components"
 
-import FrontImage from "gatsby-background-image"
+import BackgroundImage from "gatsby-background-image"
 
-const StyledHero = styled.div`
-  position: relative;
+const HeroImage = styled(BackgroundImage)`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  align-self: center;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`
+
+const StyledHero = styled.div`
   height: 100vh;
   margin-left: -12.5%;
   margin-right: -12.5%;
   background-color: blue;
 
   h1 {
-    color: var(--color-white);
     font-size: 56px;
     font-weight: 400;
+    color: var(--color-white);
     margin-bottom: 24px;
   }
 
   h2 {
-    color: var(--color-white);
     font-size: 18px;
     font-weight: 400;
     max-width: 750px;
+    color: var(--color-white);
     text-align: justify;
   }
 `
 
-const Hero = (props) => {
+const Hero = () => {
   const { t } = useTranslation()
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      file(relativePath: { eq: "portfolio-hero-2.jpg" }) {
+        id
+        childImageSharp {
+          fluid(quality: 100) {
+            src
+          }
+        }
+      }
+    }
+  `)
 
+  const imageData = data.file.childImageSharp.fluid
   return (
     <StyledHero>
-      <h1>{t("test")}</h1>
-      <h2>
-        Welcome! My name is Brecht. I am a junior javascript developer with an
-        interest in both frontend as backend work. For now, I work on personal
-        projects, but I am keen to start a real job. My personal goal is to
-        become the best web developer I can be.{" "}
-      </h2>
+      <HeroImage fluid={imageData} effect="sharpened">
+        <h1>{t("test")}</h1>
+        <h2>
+          Welcome! My name is Brecht. I am a junior Javascript developer with an
+          interest in both frontend as backend work. For now, I work on personal
+          projects, but I am keen to start a real job. My personal goal is to
+          become the best web developer I can be.
+        </h2>
+      </HeroImage>
     </StyledHero>
   )
 }
-
-export const data = useStaticQuery(graphql`
-  query HeaderQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`)
 
 export default Hero
