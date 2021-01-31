5d3b5c06954f8c14459e079f4793755c1f946491
bnulens <brechtnulens@gmail.com>
2021-01-31 18:04:21 +0100

    Omitted mapping of reviews for single review - added translation - adjusted styling for mobile

--- src/components/Content/PeerReviews.js
+++ src/components/Content/PeerReviews.js
@@ -1,75 +1,81 @@
-import React from 'react'
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { useTranslation } from "react-i18next"
 
-import { StyledPeerReview, DefaultAvatar, PeerAvatar, CompanyLogo, DefaultCompany } from "../../style/elements/experience/StyledExperience"
-import { reviewContent } from '../../info/textContents'
+import {
+  StyledPeerReview,
+  DefaultAvatar,
+  PeerAvatar,
+  CompanyLogo,
+  DefaultCompany,
+} from "../../style/elements/experience/StyledExperience"
+import { reviewContent } from "../../info/textContents"
 
 const PeerReviews = () => {
-    const data = useStaticQuery(graphql`
-        query { allCloudinaryMedia(filter: {public_id: {glob: "gatsby-portfolio/reviews/*"}}) {
-                edges {
-                    node {
-                        secure_url
-                    }
-                }
-            }
+  const { t } = useTranslation()
+  const data = useStaticQuery(graphql`
+    query {
+      allCloudinaryMedia(
+        filter: { public_id: { glob: "gatsby-portfolio/reviews/*" } }
+      ) {
+        edges {
+          node {
+            secure_url
+          }
         }
-        `
-    );
+      }
+    }
+  `)
 
-    const reviewImages = data.allCloudinaryMedia.edges;
-    const filteredUrls = []
+  const reviewImages = data.allCloudinaryMedia.edges
+  const filteredUrls = []
 
-    reviewImages.forEach((item) => {
-        filteredUrls.push(item.node.secure_url)
-    });
+  reviewImages.forEach((item) => {
+    filteredUrls.push(item.node.secure_url)
+  })
 
-    return (
-        <ul className="review__list">
-            {
-                reviewContent.map(a => {
-                    return (
-                        <StyledPeerReview key={Math.random() * 10}>
-                                <article className="review__container">
-                                    <div className="review__profile">
-                                        {a.images.avatar ? 
-                                            <PeerAvatar
-                                                cloudName="bnulens"
-                                                publicId={`gatsby-portfolio/reviews/${a.images.avatar}`}
-                                                secure="true"
-                                                alt="peer-avatar"
-                                            /> 
-                                            :
-                                            <DefaultAvatar
-                                                cloudName="bnulens"
-                                                publicId="gatsby-portfolio/reviews/default-avatar.jpg"
-                                                secure="true"
-                                                alt="default-avatar"
-                                            />
-                                        }
-                                        {a.images.company ?
-                                            <CompanyLogo
-                                                cloudName="bnulens"
-                                                publicId={`gatsby-portfolio/reviews/${a.images.company}`}
-                                                secure="true"
-                                                alt="peer-avatar"
-                                            />
-                                            :
-                                            <DefaultCompany >no company specified {String.fromCodePoint(0x2753)}</DefaultCompany>
-                                        }
-                                    </div>
-                                    <div className="review__text">
-                                        <p className="review__comment">{`"${a.comment}"`}</p>
-                                        <p className="review__person"><b>{a.reviewer}</b></p>
-                                        <p className="review__job-title">{a.jobTitle} at {a.worksAt}</p>
-                                    </div>
-                                </article>
-                        </StyledPeerReview>
-                    )
-                })
-            }
-        </ul>
-    )
-}   
+  return (
+    <StyledPeerReview>
+      <div className="review__profile">
+        {reviewContent[0].images.avatar ? (
+          <PeerAvatar
+            cloudName="bnulens"
+            publicId={`gatsby-portfolio/reviews/${reviewContent[0].images.avatar}`}
+            secure="true"
+            alt="peer-avatar"
+          />
+        ) : (
+          <DefaultAvatar
+            cloudName="bnulens"
+            publicId="gatsby-portfolio/reviews/default-avatar.jpg"
+            secure="true"
+            alt="default-avatar"
+          />
+        )}
+        {reviewContent[0].images.company ? (
+          <CompanyLogo
+            cloudName="bnulens"
+            publicId={`gatsby-portfolio/reviews/${reviewContent[0].images.company}`}
+            secure="true"
+            alt="peer-avatar"
+          />
+        ) : (
+          <DefaultCompany>
+            no company specified {String.fromCodePoint(0x2753)}
+          </DefaultCompany>
+        )}
+      </div>
+      <div className="review__text">
+        <p className="review__comment">{t(reviewContent[0].comment)}</p>
+        <p className="review__person">
+          <b>{reviewContent[0].reviewer}</b>
+        </p>
+        <p className="review__job-title">
+          {reviewContent[0].jobTitle} at {reviewContent[0].worksAt}
+        </p>
+      </div>
+    </StyledPeerReview>
+  )
+}
 
 export default PeerReviews
--- src/style/elements/experience/StyledExperience.js
+++ src/style/elements/experience/StyledExperience.js
@@ -2,174 +2,167 @@ import styled from "styled-components"
 import { Image } from "cloudinary-react"
 
 export const StyledExperience = styled.section`
-    display: block;
-    margin: 56px 0;
+  display: block;
+  margin: 56px 0;
+
+  .experience__container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    .experience__header {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+
+      h3 {
+        font-size: 4vw;
+        font-weight: 400;
+      }
+
+      p {
+        width: 75%;
+        margin-top: 12px;
+        color: var(--color-silver);
+      }
+    }
+
+    .experience__links {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 24px auto 0px;
+      width: 50%;
+
+      a.link__record-store {
+        color: white;
+        border-radius: 4px;
+        background-color: var(--color-purple);
+        width: 100%;
+        padding: 8px;
+        margin-right: 12px;
+        text-align: center;
+        cursor: pointer;
+      }
+
+      a.link__motivation {
+        color: var(--color-purple);
+        border: 1px solid var(--color-purple);
+        border-radius: 4px;
+        width: 100%;
+        padding: 8px;
+        margin-left: 12px;
+        text-align: center;
+      }
+    }
+  }
+  @media ${(props) => props.theme.mediaQueries.medium} {
+    margin: 8vh 0;
 
     .experience__container {
-        display: flex;
-        flex-direction: column;
+      .experience__header {
+        text-align: justify;
+
+        h3 {
+          font-size: 8vw;
+        }
+        p {
+          width: 100%;
+        }
+      }
+      .experience__links {
         width: 100%;
-
-        .experience__header {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            text-align: center;
-
-            h3 {
-                font-size: 4vw;
-                font-weight: 400;
-            }
-
-            p {
-                width: 75%;
-                margin-top: 12px;
-                color: var(--color-silver);
-            }
-        }
-
-        .experience__links {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            margin: 24px auto 0px;
-            width: 50%;
-
-            a.link__record-store {
-                color: white;
-                border-radius: 4px;
-                background-color: var(--color-purple);
-                width: 100%;
-                padding: 8px;
-                margin-right: 12px;
-                text-align: center;
-                cursor: pointer;
-            }
-
-            a.link__motivation {
-                color: var(--color-purple);
-                border: 1px solid var(--color-purple);
-                border-radius: 4px;
-                width: 100%;
-                padding: 8px;
-                margin-left: 12px;
-                text-align: center;
-            }
-        }
+      }
     }
-    @media ${(props) => props.theme.mediaQueries.medium} {
-        margin: 8vh 0;
-
-        .experience__container {
-            .experience__header {
-                text-align: justify;
-
-                h3 {
-                    font-size: 8vw;
-                }
-                p {
-                    width: 100%;
-                }
-            }
-            .experience__links {
-                width: 100%;
-            }
-        }
-    }
+  }
 `
 
-export const StyledPeerReview = styled.li`
-    max-width: 75%;
-    margin: 48px auto;
-    -webkit-box-shadow: 0px 0px 18px -4px #E2E2E2;
-    -moz-box-shadow: 0px 0px 18px -4px #E2E2E2;
-    box-shadow: 0px 0px 18px -4px #E2E2E2;
+export const StyledPeerReview = styled.article`
+  display: flex;
+  flex-direction: row;
+  width: 75%;
+  padding: 24px;
+  margin: 0 auto;
+  color: var(--color-grey);
+  max-width: 75%;
+  margin: 48px auto;
+  -webkit-box-shadow: 0px 0px 18px -4px #e2e2e2;
+  -moz-box-shadow: 0px 0px 18px -4px #e2e2e2;
+  box-shadow: 0px 0px 18px -4px #e2e2e2;
 
-    .review__container {
-        display: flex;
-        flex-direction: row;
-        width: 75%;
-        padding: 24px;
-        margin: 0 auto;
-        color: var(--color-grey);
+  .review__profile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 30%;
+    padding: 16px;
 
-        .review__profile {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            width: 30%;
-            padding: 16px;
-
-            img:first-child {
-                display: block;
-                width: 100px;
-                height: 100px;
-                border-radius: 50%;
-            }
-
-            img:last-child {
-                display: block;
-                width: 100px;
-                height: 50px;
-                margin-top: 12px;
-            }
-        }
-
-        .review__text {
-            border-left: 2px solid var(--color-lightgrey);
-            width: 70%;
-            padding: 16px;
-
-            .review__comment {
-                font-style: italic;
-                text-align: justify;
-                color: grey;
-            }
-            .review__person {
-                font-size: 12px;
-                margin-top: 16px;
-            }
-            .review__job-title {
-                font-size: 12px;
-            }
-        }
+    img:first-child {
+      display: block;
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
     }
 
-    @media ${(props) => props.theme.mediaQueries.medium} {
-        margin: 10vh auto;
-        max-width: 100%;
-
-        .review__container {
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-
-            .review__profile {
-                width: 100%;
-            }
-
-            .review__text {
-                border-top: 2px solid var(--color-lightgrey);
-                border-left: none;
-                width: 100%;
-            }
-        }
+    img:last-child {
+      display: block;
+      width: 100px;
+      height: 50px;
+      margin-top: 12px;
     }
+  }
+
+  .review__text {
+    border-left: 2px solid var(--color-lightgrey);
+    width: 70%;
+    padding: 16px;
+
+    .review__comment {
+      font-style: italic;
+      text-align: justify;
+      color: grey;
+    }
+    .review__person {
+      font-size: 12px;
+      margin-top: 16px;
+    }
+    .review__job-title {
+      font-size: 12px;
+    }
+  }
+
+  @media ${(props) => props.theme.mediaQueries.medium} {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin: 10vh auto;
+    max-width: 100%;
+
+    .review__profile {
+      width: 100%;
+    }
+
+    .review__text {
+      border-top: 2px solid var(--color-lightgrey);
+      border-left: none;
+      width: 100%;
+    }
+  }
 `
-
 
 export const DefaultAvatar = styled(Image)`
-    display: block;
+  display: block;
 `
 export const DefaultCompany = styled.span`
-    display: block;
-    font-size: 12px;
+  display: block;
+  font-size: 12px;
 `
 export const PeerAvatar = styled(Image)`
-    object-fit: contain;
+  object-fit: contain;
 `
 export const CompanyLogo = styled(Image)`
-    object-fit: contain;
+  object-fit: contain;
 `
