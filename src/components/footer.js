9e0a59e76993f523d4fb9335c5b0b37caf4c276b
bnulens <brechtnulens@gmail.com>
2021-01-31 17:54:12 +0100

    Omitted navLinks to separate file - adjusted styling for mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,49 +1,40 @@
-import React from 'react'
+import React from "react"
 
-import { StyledFooter } from '../style/elements/StyledFooter'
+import { StyledFooter } from "../style/elements/StyledFooter"
+import { navLinks } from "../info/textContents"
 
-const thisYear = new Date().getFullYear();
-
-const navLinks = {
-    social: [
-      {
-        title: 'linkedIn',
-        url: 'https://www.linkedin.com/in/brecht-nulens/'
-      },{
-        title: 'github',
-        url: 'https://github.com/bnulens'
-      },
-      {
-        title: 'email',
-        url:'mailto:brechtnulens@gmail.com'
-      }
-    ],
-    site: ['about','skills','experience','contact']
-};
+const thisYear = new Date().getFullYear()
 
 const Footer = () => (
-    <StyledFooter>
-        <div className="footer__inner-wrapper">
-            <section className="footer__brief">
-                <h3>Alley-Oop</h3>
-                <p>Here I say something</p>
-            </section>
-            <section className="footer__social-media">
-                <nav>
-                    {
-                        navLinks.social.map((item) => {
-                            return (
-                                <a className="footer-link" href={item.url} target="__blank" rel="norefferer noopener" key={item.title}>
-                                    <img className="footer__social-icon" src={`../../svg/${item.title}.svg`} alt={item.title}/>
-                                </a>
-                            )
-                        })
-                    }
-                </nav>
-                <span>&copy; {thisYear}</span>
-            </section>
-        </div>
-    </StyledFooter>
+  <StyledFooter>
+    <div className="footer__wrapper">
+      <section className="footer__brief">
+        <h3>Alley-Oop</h3>
+      </section>
+      <section className="footer__social-media">
+        <nav>
+          {navLinks.social.map((item) => {
+            return (
+              <a
+                className="footer-link"
+                href={item.url}
+                target="__blank"
+                rel="norefferer noopener"
+                key={item.title}
+              >
+                <img
+                  className="footer__social-icon"
+                  src={`../../svg/${item.title}.svg`}
+                  alt={item.title}
+                />
+              </a>
+            )
+          })}
+        </nav>
+        <span>&copy; {thisYear}</span>
+      </section>
+    </div>
+  </StyledFooter>
 )
 
-export default Footer 
+export default Footer
--- src/style/elements/StyledFooter.js
+++ src/style/elements/StyledFooter.js
@@ -1,48 +1,54 @@
 import styled from "styled-components"
 
 export const StyledFooter = styled.footer`
-    height: 300px;
-    width: 100%;
-    padding: 32px;
-    color: white;
-    background-color: var(--color-coal);
+  height: 300px;
+  padding: 32px;
+  color: white;
+  background-color: var(--color-coal);
 
-    .footer__inner-wrapper {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        width: 80vw;
-        margin: 0 auto;
+  .footer__wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin: 0 auto;
+    height: 100%;
+    max-width: 80%;
 
-        .footer__brief {
-            h3 {
-                font-size: 40px;
-                font-weight: 100;
-                text-decoration: underline;
-            }
+    .footer__brief {
+      h3 {
+        font-size: 40px;
+        font-weight: 100;
+        text-decoration: underline;
+      }
 
-            p {
-                font-weight: 100;
-            }
-        }
-
-        .footer__social-media {
-            nav {
-                display: flex;
-                justify-content: space-between;
-                width: 100px;
-
-                a.footer-link {
-                    display: block;
-                    width: 10px;
-
-                    img.footer__social-icon {
-                        display: block;
-                        width: 25px;
-                    }
-                }
-            }
-        }
+      p {
+        font-weight: 100;
+      }
     }
+
+    .footer__social-media {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+
+      nav {
+        display: flex;
+        justify-content: space-between;
+
+        a.footer-link {
+          display: block;
+          margin-right: 12px;
+
+          img.footer__social-icon {
+            display: block;
+            width: 20px;
+          }
+        }
+      }
+    }
+  }
+  @media ${(props) => props.theme.mediaQueries.medium} {
+  }
 `
