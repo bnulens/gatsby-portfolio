57d314f2ff260073baa048e3e0d90a68db0e145f
bnulens <brechtnulens@gmail.com>
2021-01-31 18:00:16 +0100

    Added translations to section id and content

--- src/components/Content/Experience.js
+++ src/components/Content/Experience.js
@@ -2,23 +2,39 @@ import React from "react"
 
 import PeerReviews from "./PeerReviews"
 import { StyledExperience } from "../../style/elements/experience/StyledExperience"
+import { useTranslation } from "react-i18next"
 
 const Experience = () => {
-    return (
-        <StyledExperience id="experience">
-            <div className="experience__container">
-                <div className="experience__header">
-                    <h3>Ready to rebound</h3>
-                    <p>After a couple attempts in finding a proper field of education, I am convinced I now am on the right track. I feel ready to strike and make this the greatest rebound of my life.</p>
-                </div>
-                <div className="experience__links">
-                    <a className="link__record-store" href="https://epic-galileo-8a7f26.netlify.app/" target="_blank" rel="noopener noreferrer">Record store</a>
-                    <a className="link__motivation" href="https://unruffled-bartik-c63ef3.netlify.app/" target="_blank" rel="noopener noreferrer">Motivation</a>
-                </div>
-            </div>
-            <PeerReviews/>
-        </StyledExperience>
-    )
+  const { t } = useTranslation()
+  return (
+    <StyledExperience id={t("navbar.experience")}>
+      <div className="experience__container">
+        <div className="experience__header">
+          <h3>{t("experience.headingTitle")}</h3>
+          <p>{t("experience.heading")}</p>
+        </div>
+        <div className="experience__links">
+          <a
+            className="link__record-store"
+            href="https://epic-galileo-8a7f26.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t("experience.links.recordStore")}
+          </a>
+          <a
+            className="link__motivation"
+            href="https://unruffled-bartik-c63ef3.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t("experience.links.motivation")}
+          </a>
+        </div>
+      </div>
+      <PeerReviews />
+    </StyledExperience>
+  )
 }
 
 export default Experience
