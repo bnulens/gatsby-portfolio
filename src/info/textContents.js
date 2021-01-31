8c1d7ba154362e3022fd987ee390d867cf343b04
bnulens <brechtnulens@gmail.com>
2021-01-31 18:11:41 +0100

    Paragraphs & comments moved to i18n  translations files

--- src/info/textContents.js
+++ src/info/textContents.js
@@ -1,84 +1,85 @@
 export const reviewContent = [
   {
     reviewer: "Geert Van Campenhout",
-    comment: "I have worked with Brecht on two projects. He is eager to learn and a high potential. In the short amount of time he has been developing, he has picked up a lot of skills and is on track to become a great addition to any development team.",
+    comment: "review.comment1",
     jobTitle: "Full stack developer",
     worksAt: "StriveCloud - scalable gamification software",
     images: {
       avatar: "geert-lion.jpg",
-      company: "strivecloud-full.png"
-    }
-  }
-];
+      company: "strivecloud-full.png",
+    },
+  },
+]
 
 export const listContent = [
-    {
-      tag: "newbie",
-      title: "I am a noob",
-      comment: "Hello"
-    },
-    {
-      tag: "education",
-      title: "Academics",
-      comment: "A road less travelled...",
-      studies: [
-        {
-          title: 'Junior Web Developer',
-          year: '2019',
-          place: 'Genk',
-          organistation: 'BeCode',
-          success: true
-        },
-        {
-          title: 'Junior Network Engineer',
-          year: '2018',
-          place: 'Genk',
-          organistation: 'Syntra',
-          success: true
-        },
-        {
-          title: 'Bachelor Graphic Design',
-          year: '2014-2018',
-          place: 'Hasselt',
-          organistation: 'PXL Mad-Faculty',
-          success: false
-        },
-        {
-          title: 'Bachelor Product Design',
-          year: '2013-2014',
-          place: 'Genk',
-          organistation: 'PXL Mad-Faculty',
-          success: false
-        },
-        {
-          title: 'ASO',
-          year: '2007-2013',
-          place: 'Hasselt',
-          organistation: 'Koninklijk Atheneum 1',
-          success: true
-        },
-      ]
-    },
-    {
-      tag: "extra",
-      title: "Possibility",
-      comment: "A quick brown fox jumped over the lazy dog"
-    },
-];
-
-export const navLinks = {
-    social: [
+  {
+    tag: "about.education",
+    title: "about.academics",
+    comment: "about.comments.item",
+    studies: [
       {
-        title: 'linkedIn',
-        url: 'https://www.linkedin.com/in/brecht-nulens/'
-      },{
-        title: 'github',
-        url: 'https://github.com/bnulens'
+        title: "Junior Web Developer",
+        year: "2019",
+        place: "Genk",
+        organisation: "BeCode",
+        success: true,
       },
       {
-        title: 'email',
-        url:'mailto:brechtnulens@gmail.com'
-      }
+        title: "Junior Network Engineer",
+        year: "2018",
+        place: "Genk",
+        organisation: "Syntra",
+        success: true,
+      },
+      {
+        title: "Bachelor Graphic Design",
+        year: "2014-2018",
+        place: "Hasselt",
+        organisation: "PXL Mad-Faculty",
+        success: false,
+      },
+      {
+        title: "Bachelor Product Design",
+        year: "2013-2014",
+        place: "Genk",
+        organisation: "PXL Mad-Faculty",
+        success: false,
+      },
+      {
+        title: "Latin - Modern Languages",
+        year: "2007-2013",
+        place: "Hasselt",
+        organisation: "Koninklijk Atheneum",
+        success: true,
+      },
     ],
-    site: ['about','experience']
-};
+  },
+  {
+    tag: "newbie",
+    title: "about.skills",
+    comment: "about.comments.item1",
+  },
+  {
+    tag: "extra",
+    title: "about.interests",
+    comment: "about.comments.item2",
+  },
+]
+
+export const navLinks = {
+  social: [
+    {
+      title: "linkedIn",
+      url: "https://www.linkedin.com/in/brecht-nulens/",
+    },
+    {
+      title: "github",
+      url: "https://github.com/bnulens",
+    },
+    {
+      title: "email",
+      url: "mailto:brechtnulens@gmail.com",
+    },
+  ],
+  site: ["navbar.about", "navbar.experience"],
+}
