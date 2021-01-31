b9325443deb30422dc4aeb61f81e1085239e38a5
bnulens <brechtnulens@gmail.com>
2021-01-31 18:10:04 +0100

    Adjusted styling for mobile - changed query for hero image - check if there is a hero image available - added translation

--- src/components/Content/Hero.js
+++ src/components/Content/Hero.js
@@ -6,37 +6,43 @@ import styled from "styled-components"
 import BackgroundImage from "gatsby-background-image"
 
 const HeroImage = styled(BackgroundImage)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  align-self: center;
   width: 100%;
   height: 100%;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+
+  .hero-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    align-self: center;
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
+    max-width: 80%;
+  }
 `
 
 const StyledHero = styled.div`
-  height: 100vh;
+  height: 650px;
   margin-left: -12.5%;
   margin-right: -12.5%;
   background-color: blue;
+  color: var(--color-white);
 
   h1 {
-    font-size: 56px;
+    font-size: 72px;
     font-weight: 400;
-    color: var(--color-white);
     margin-bottom: 24px;
+    line-height: 72px;
   }
 
   h2 {
     font-size: 18px;
     font-weight: 400;
-    max-width: 750px;
-    color: var(--color-white);
-    text-align: justify;
+    max-width: 350px;
   }
 `
 
@@ -44,28 +50,35 @@ const Hero = () => {
   const { t } = useTranslation()
   const data = useStaticQuery(graphql`
     query MyQuery {
-      file(relativePath: { eq: "portfolio-hero-2.jpg" }) {
+      file(relativePath: { eq: "portfolio-hero.jpg" }) {
         id
         childImageSharp {
           fluid(quality: 100) {
-            src
+            ...GatsbyImageSharpFluid
           }
         }
       }
     }
   `)
 
+  if (!data?.file?.childImageSharp?.fluid) {
+    return (
+      <StyledHero>
+        <h1>{t("header.mainTitle")}</h1>
+        <h2>{t("header.catchTitle")}</h2>
+      </StyledHero>
+    )
+  }
+
   const imageData = data.file.childImageSharp.fluid
+
   return (
     <StyledHero>
       <HeroImage fluid={imageData} effect="sharpened">
-        <h1>{t("test")}</h1>
-        <h2>
-          Welcome! My name is Brecht. I am a junior Javascript developer with an
-          interest in both frontend as backend work. For now, I work on personal
-          projects, but I am keen to start a real job. My personal goal is to
-          become the best web developer I can be.
-        </h2>
+        <div className="hero-wrapper">
+          <h1>{t("header.mainTitle")}</h1>
+          <h2>{t("header.catchTitle")}</h2>
+        </div>
       </HeroImage>
     </StyledHero>
   )
