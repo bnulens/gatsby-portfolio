dbd26252e204a8acc68f9d956fb2c1d333fe7533
bnulens <brechtnulens@gmail.com>
2021-01-31 17:56:04 +0100

    Omitted styling to StyledHeader file - added translation to navbar elements and hrefs

--- src/components/header.js
+++ src/components/header.js
@@ -1,89 +1,36 @@
 import React from "react"
-import styled from "styled-components"
 import { Link } from "gatsby"
+import styled from "styled-components"
+import { useTranslation } from "react-i18next"
 
 import { navLinks } from "../info/textContents"
+import { StyledHeader } from "../style/elements/StyledHeader"
 
 import LanguageSwitcher from "./LanguageSwitcher"
 
-const Nav = styled.header`
-  position: absolute;
-  top: 6%;
-  left: 10%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  max-width: 80vw;
-  z-index: 100;
-`
-const NavListSocial = styled.ul`
-  display: flex;
-  align-items: center;
-  height: 100%;
-
-  a {
-    display: block;
-    margin-right: 24px;
-    cursor: pointer;
-
-    img {
-      display: block;
-      height: 20px;
-      width: auto;
-      transition: 0.4s all ease;
-
-      :hover {
-        filter: invert(1);
-      }
-    }
-  }
-`
-const NavList = styled.ul`
-  display: flex;
-  flex-direction: ${(props) => (props.mobile ? "column" : "row")};
-  align-items: center;
-  height: 100%;
-`
 const NavLink = styled(Link)`
   color: var(--color-white);
   margin-left: 24px;
 `
 
-const NavItems = () => {
+const Header = () => {
+  const { t } = useTranslation()
   return (
-    <Nav>
-      <NavListSocial>
-        {navLinks.social.map((item) => {
-          return (
-            <a
-              className="header__social-link"
-              href={item.url}
-              target="__blank"
-              rel="norefferer noopener"
-              key={item.title}
-            >
-              <img
-                className="header__social-icon"
-                src={`../../svg/${item.title}.svg`}
-                alt={item.title}
-              />
-            </a>
-          )
-        })}
-      </NavListSocial>
-      <LanguageSwitcher />
-      <NavList>
-        {navLinks.site.map((item) => {
-          return (
-            <NavLink to={`#${item}`} key={item}>
-              {item}
-            </NavLink>
-          )
-        })}
-      </NavList>
-    </Nav>
+    <StyledHeader>
+      <div className="header__inner-wrapper">
+        <LanguageSwitcher />
+        <nav className="header__site-nav">
+          {navLinks.site.map((item) => {
+            return (
+              <NavLink to={`#${t(item)}`} key={t(item)}>
+                {t(item)}
+              </NavLink>
+            )
+          })}
+        </nav>
+      </div>
+    </StyledHeader>
   )
 }
 
-export default NavItems
+export default Header
--- src/style/elements/StyledHeader.js
+++ src/style/elements/StyledHeader.js
@@ -1,58 +1,63 @@
 import styled from "styled-components"
 
 export const StyledHeader = styled.header`
-    position: absolute;
-    left: 10%;
-    height: 100px;
-    z-index: 5;
+  position: absolute;
+  top: 6%;
+  left: 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 80vw;
+  z-index: 100;
 
-    .header__inner-wrapper {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 100%;
-        width: 80vw;
-        margin: 0 auto;
+  .header__inner-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    width: 80vw;
+    margin: 0 auto;
 
-        nav.header__social-nav {
-            display: flex;
-            width: 50%;
-            a.header__social-link {
-                margin-right: 20px;
-                img.header__social-icon {
-                    display: block;
-                    height: 30px;
-                    width: 30px;
-                    transition: filter ease 0.4s;
+    nav.header__social-nav {
+      display: flex;
+      width: 50%;
+      a.header__social-link {
+        margin-right: 20px;
+        img.header__social-icon {
+          display: block;
+          height: 30px;
+          width: 30px;
+          transition: filter ease 0.4s;
 
-                    :hover {
-                        filter: invert(100%);
-                    }
-                }
-            }
+          :hover {
+            filter: invert(100%);
+          }
         }
-
-        .header__site-nav {
-            display: flex;
-            justify-content: flex-end;
-            width: 50%;
-
-            a.header__site-nav-link {
-                position: relative;
-                width: auto;
-                margin-right: 14px;
-                padding: 8px;
-                color: white;
-                background: linear-gradient(to left, transparent 50%, black 50%);
-                background-size: 200% 100%;
-                background-position: right bottom;
-                transition: all ease-in 0.4s;
-                
-                :hover {
-                    color: white;
-                    background-position: left bottom;
-                }
-            }
-        }
+      }
     }
+
+    .header__site-nav {
+      display: flex;
+      justify-content: flex-end;
+      width: 50%;
+
+      a.header__site-nav-link {
+        position: relative;
+        width: auto;
+        margin-right: 14px;
+        padding: 8px;
+        color: white;
+        background: linear-gradient(to left, transparent 50%, black 50%);
+        background-size: 200% 100%;
+        background-position: right bottom;
+        transition: all ease-in 0.4s;
+
+        :hover {
+          color: white;
+          background-position: left bottom;
+        }
+      }
+    }
+  }
 `
