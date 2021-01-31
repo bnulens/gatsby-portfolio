16342b2de896927fd4f7a6ff9d86d87765d8faed
bnulens <brechtnulens@gmail.com>
2021-01-31 18:05:29 +0100

    Added translation to section id & content

--- src/components/Content/ScrollProgress.js
+++ src/components/Content/ScrollProgress.js
@@ -1,45 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+import { useTranslation } from "react-i18next"
 
 const ProgressBar = styled.div`
-    position: sticky;
-    display: block;
-    top: 0;
-    height: 4px;
-    width: 100%;
-    z-index: 100;
-    --scroll: 0%;
-    background: linear-gradient(90deg, var(--color-purple) var(--scroll), transparent 0);
+  position: sticky;
+  display: block;
+  top: 0;
+  height: 4px;
+  width: 100%;
+  z-index: 100;
+  --scroll: 0%;
+  background-color: blue;
 `
 
 const ScrollProgress = () => {
+  const { t } = useTranslation()
 
-    const [progress, setProgress] = useState(0)
+  const [progress, setProgress] = useState(0)
 
-    useEffect(() => {
-        const catchProgress = e => {
-            if (typeof window !== undefined) {
-                e.preventDefault();
-                const aboutSection = document.getElementById("about");
-                const progressBar = document.getElementById("about-scroll__progress");
-                const aboutRect = aboutSection.getBoundingClientRect();
+  useEffect(() => {
+    const catchProgress = (e) => {
+      if (typeof window !== undefined) {
+        e.preventDefault()
+        const aboutSection = document.getElementById(t("navbar.about"))
+        const progressBar = document.getElementById("about-scroll__progress")
+        const aboutRect = aboutSection.getBoundingClientRect()
 
-                if(aboutRect.top < 0 && aboutRect.bottom >= document.documentElement.clientHeight) {
-                    const progress = Math.floor(aboutRect.top / aboutSection.clientHeight * -145) + "%";
-                    setProgress(progress);
-                    progressBar.style.setProperty("--scroll", progress);
-                }
-            }
+        if (
+          aboutRect.top < 0 &&
+          aboutRect.bottom >= document.documentElement.clientHeight
+        ) {
+          const progress =
+            Math.floor((aboutRect.top / aboutSection.clientHeight) * -145) + "%"
+          setProgress(progress)
+          progressBar.style.setProperty("--scroll", progress)
         }
-    
-        window.addEventListener("scroll", catchProgress, { passive: false });
-        return () => window.removeEventListener("scroll", catchProgress);
+      }
+    }
 
-    }, [progress, setProgress])
+    window.addEventListener("scroll", catchProgress, { passive: false })
+    return () => window.removeEventListener("scroll", catchProgress)
+  }, [progress, setProgress])
 
-    return (
-        <ProgressBar id="about-scroll__progress"></ProgressBar>
-    )
+  return <ProgressBar id="about-scroll__progress"></ProgressBar>
 }
 
 export default ScrollProgress
