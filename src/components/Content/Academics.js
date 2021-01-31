c6b63dd244efcf2540c57f88251887de691b2390
bnulens <brechtnulens@gmail.com>
2021-01-31 17:57:50 +0100

    Basic styling for education items

--- src/components/Content/Academics.js
+++ src/components/Content/Academics.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from "react"
+import styled from "styled-components"
 
+const StyledItem = styled.li`
+  margin-top: 24px;
+`
 
 const Academics = ({ education }) => {
-    return (
-        <ul className="list-item__studies">
-            {
-                education.map(s => {
-                    return (
-                        <li className="list-item__study-item" key={s.title}>
-                            <div className="list-item__study-item-header">
-                                <p>{s.year}</p>
-                                <h3>{s.title}</h3>
-                            </div>
-                            <div className="list-item__study-item-content">
-                                <p>{s.place}</p>
-                                <p>{s.organisation}</p>
-                                <p>{s.success}</p>
-                            </div>
-
-                        </li>
-                    )
-                })
-            }
-        </ul>
-    )
+  return (
+    <ul className="list-item__studies">
+      {education.map((s) => {
+        return (
+          <StyledItem key={s.title}>
+            <div className="list-item__study-item-header">
+              <h3>{s.title}</h3>
+            </div>
+            <div className="list-item__study-item-content">
+              <p>
+                {s.place} - {s.organisation}
+              </p>
+              <p>{s.success}</p>
+            </div>
+          </StyledItem>
+        )
+      })}
+    </ul>
+  )
 }
 
 export default Academics
