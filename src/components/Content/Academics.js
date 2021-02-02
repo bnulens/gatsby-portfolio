ccf367d5e952491d985ba6aa384b9c1f127cc2d4
bnulens <brechtnulens@gmail.com>
2021-02-02 20:48:42 +0100

    Imported svg via path - restructured list-item__study-item - display icon on degree success

--- src/components/Content/Academics.js
+++ src/components/Content/Academics.js
@@ -1,34 +1,56 @@
 import React from "react"
+import { useTranslation } from "react-i18next"
 import styled from "styled-components"
+
+import Graduation from "../../icons/graduation2.svg"
 
 const StyledItem = styled.li`
   margin-top: 32px;
   font-weight: 100;
 
-  h4 {
-    font-size: 24px;
-    font-weight: 100;
+  .list-item__study-item-wrapper {
+    display: flex;
+    width: 100%;
+    .list-item__study-item-content {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      h4 {
+        font-size: 24px;
+        font-weight: 100;
+        margin-left: 20px;
+        text-align: left;
+      }
+      p {
+        color: var(--color-silver);
+      }
+    }
   }
+`
 
-  p {
-    color: var(--color-silver);
+const StyledHat = styled(Graduation)`
+  display: block;
+  width: 32px;
+  path {
+    fill: var(--color-silver);
   }
 `
 
 const Academics = ({ education }) => {
+  const { t } = useTranslation()
   return (
     <ul className="list-item__studies">
       {education.map((s) => {
         return (
           <StyledItem key={s.title}>
-            <div className="list-item__study-item-header">
-              <h4>{s.title}</h4>
-            </div>
-            <div className="list-item__study-item-content">
-              <p>
-                {s.place} - {s.organisation}
-              </p>
-              <p>{s.success}</p>
+            <div className="list-item__study-item-wrapper">
+              {s.success ? <StyledHat /> : null}
+              <div className="list-item__study-item-content">
+                <h4>{s.title}</h4>
+                <p>
+                  {s.place} - {s.organisation}
+                </p>
+              </div>
             </div>
           </StyledItem>
         )
