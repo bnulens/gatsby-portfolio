66018ad82145fccad950de9e7cacb01c94284c9a
bnulens <brechtnulens@gmail.com>
2021-02-02 17:36:09 +0100

    Created class toggle on language buttons- restyled buttons for active-inactive state

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,6 +1,6 @@
-import React from "react"
-import i18next from "i18next"
+import React, { useState } from "react"
 import styled from "styled-components"
+import i18next from "i18next"
 
 const Wrapper = styled.div`
   display: inline-block;
@@ -10,38 +10,60 @@ const Wrapper = styled.div`
 
 const LanguageBlock = styled.button`
   margin-right: 6px;
-  padding: 4px;
+  padding: 4px 5px;
   border-radius: 50%;
-  border: 1px solid white;
   background: transparent;
   color: var(--color-white);
   transition: all ease 0.3s;
+  cursor: pointer;
 
   :hover {
-    background-color: var(--color-white);
-    color: var(--color-black);
+    background-color: var(--color-purple);
+  }
+
+  &.active {
+    border: 1px solid var(--color-white);
+  }
+  &.inactive {
+    color: var(--color-silver);
+    border: 1px solid var(--color-silver);
+    filter: grayscale(0.5);
   }
 `
 
 const LanguageSwitcher = () => {
+  const setLanguage = localStorage.getItem("i18nextLng")
+  const [active, setActive] = useState(setLanguage)
+
   const handleLanguageSwitch = (language) => {
-    console.log("clicked", language)
     i18next.changeLanguage(language, (err, t) => {
       if (err) return console.log("something went wrong loading", err)
     })
   }
 
+  const toggleLanguage = (e) => {
+    e.preventDefault()
+    setActive(e.target.value)
+    handleLanguageSwitch(e.target.value)
+  }
+
   return (
     <Wrapper>
       <LanguageBlock
-        className="language-button"
-        onClick={() => handleLanguageSwitch("nl")}
+        className={`language-button-active ${
+          active === "nl" ? "active" : "inactive"
+        }`}
+        onClick={(e) => toggleLanguage(e)}
+        value="nl"
       >
         NL
       </LanguageBlock>
       <LanguageBlock
-        className="language-button"
-        onClick={() => handleLanguageSwitch("en")}
+        className={`language-button-active ${
+          active === "en" ? "active" : "inactive"
+        }`}
+        onClick={(e) => toggleLanguage(e)}
+        value="en"
       >
         EN
       </LanguageBlock>
