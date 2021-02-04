c017fc491b44385eae4a1130f248b3dfb008e8dc
bnulens <brechtnulens@gmail.com>
2021-02-04 21:32:02 +0100

    Removed margins of Hero

--- src/components/Content/Hero.js
+++ src/components/Content/Hero.js
@@ -28,8 +28,6 @@ const HeroImage = styled(BackgroundImage)`
 const StyledHero = styled.div`
   height: 650px;
   min-height: 100vh;
-  margin-left: -12.5%;
-  margin-right: -12.5%;
   background-color: blue;
   color: var(--color-white);
 
